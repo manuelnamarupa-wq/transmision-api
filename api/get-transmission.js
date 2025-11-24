f94aed4cca206c24aa72784fe5952a34be8ef0a2
manuelnamarupa-wq <manuelnamarupa@gmail.com>
2025-11-24 10:03:41 -0600

    Update get-transmission.js

--- api/get-transmission.js
+++ api/get-transmission.js
@@ -2,7 +2,7 @@
 
 // 1. URL de nuestra base de datos JSON en GitHub.
 //    ¡IMPORTANTE! Verifica que tu usuario y nombre de repo sean correctos.
-const DATA_URL = 'https://raw.githubusercontent.com/manuelnamarupa-wq/transmision-api/main/transmissions.json';
+const DATA_URL = 'https://raw.githubusercontent.com/manuelnamarupa-wq/transmision-api/main/api/transmissions.json';
 
 // Variable para guardar los datos en memoria.
 let transmissionData = [];
